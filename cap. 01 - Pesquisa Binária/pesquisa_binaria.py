19b99c8097cd1d04e75248db4697b89966569fe1
SassaTricolado <contatos.samuel@outlook.com>
2024-01-05 09:38:18 -0300

    Pequena correção de nomes de variáveis

--- cap. 01 - Pesquisa Binária/pesquisa_binaria.py
+++ cap. 01 - Pesquisa Binária/pesquisa_binaria.py
@@ -1,17 +1,17 @@
 import math
 
 def pesquisa_binaria(lista, item):
-   baixo = 0
-   alto = len(lista) - 1
-   while baixo <= alto:
-      meio = math.ceil((baixo + alto) / 2)
+   inicio = 0
+   fim = len(lista) - 1
+   while inicio <= fim:
+      meio = math.ceil((inicio + fim) / 2)
       chute = lista[meio]
       if chute == item:
          return meio
       if chute > item:
-         alto = meio - 1
+         fim = meio - 1
       else:
-         baixo = meio + 1
+         inicio = meio + 1
    return None
 
 minha_lista = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39]
